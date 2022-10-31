e73b4f71efa8bc0d8b4be91b4e40f2bb18596cb0
Jos Vlaar <github.com@josvlaar.com>
2022-10-31 09:12:13 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,29 @@
 import sqlite3
-dbconnection = sqlite3.connect('sqlite.db')
-cursor = dbconnection.cursor()
-cursor.execute('CREATE TABLE users(id PRIMARY KEY, name, avgscore)')
-cursor.execute('CREATE TABLE questions(id PRIMARY KEY, question, answer, avgtime, avgscore)')
-cursor.execute('CREATE TABLE answers(user, question, answer, timeelapsed)')
+from sqlite3 import Error
+
+def create_connection(db_file):
+    con = None
+    try:
+        con = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if con:
+            con.close()
+
+if __name__ == '__main__':
+    create_connection('sqlite.db')
+
+
+
+
+
+
+"""
+con = sqlite3.connect('sqlite.db')
+cur = con.cursor()
+cur.execute('CREATE TABLE users(id PRIMARY KEY, name, avgtime, NA)')
+cur.execute('CREATE TABLE questions(id PRIMARY KEY, question, AA, AB, AC, AD, CA, avgtime, NA)')
+cur.execute('CREATE TABLE answers(id PRIMARY KEY, user_id, question_id, answer, timeelapsed, factor)')
+"""
