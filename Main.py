e3f5a677322bde7e4cb07cb693e2ae6c3aaf90ec
Jos Vlaar <github.com@josvlaar.com>
2022-11-17 12:59:10 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,38 +7,40 @@ mydb = mysql.connector.connect(
   database="test"
 )
 
-mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE users(name VARCHAR(255))")
-
-
-
 sql_create_users = """
     CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name text NOT NULL,
-        avgtime integer,
-        numofquestions integer
+        name TEXT NOT NULL,
+        avgtime INT,
+        numofquestions INT
     ) """
 sql_create_questions = """
     CREATE TABLE IF NOT EXISTS questions (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        question text NOT NULL,
-        answerA text NOT NULL,
-        answerB text NOT NULL,
-        answerC text NOT NULL,
-        answerD text NOT NULL,
-        correctanswer text NOT NULL,
-        avgtime integer NOT NULL,
-        numofanswers integer
+        question TEXT NOT NULL,
+        answerA TEXT NOT NULL,
+        answerB TEXT NOT NULL,
+        answerC TEXT NOT NULL,
+        answerD TEXT NOT NULL,
+        correctanswer CHAR NOT NULL,
+        avgtime INT NOT NULL,
+        numofanswers INT
     ) """
 sql_create_answers = """
     CREATE TABLE IF NOT EXISTS answers (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id integer NOT NULL,
-        question_id integer NOT NULL,
-        answer text NOT NULL,
-        timeelapsed integer NOT NULL,
-        factor real NOT NULL,
+        user_id INT NOT NULL,
+        question_id INT NOT NULL,
+        answer CHAR NOT NULL,
+        timeelapsed INT NOT NULL,
+        factor FLOAT NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id),
         FOREIGN KEY (question_id) REFERENCES questions(id)
     ) """
+
+mycursor = mydb.cursor()
+mycursor.execute(sql_create_users)
+mycursor.execute(sql_create_questions)
+mycursor.execute(sql_create_answers)
+
+user = input("Type your user name: ")
