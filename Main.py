2e832011f1a73c6d41c2c90e63499ef14f64acd7
Jos Vlaar <github.com@josvlaar.com>
2022-11-30 21:15:14 +0100

    Changes

--- Main.py
+++ Main.py
@@ -22,44 +22,41 @@ sql_create_questions = """
     CREATE TABLE IF NOT EXISTS questions (
         id INT AUTO_INCREMENT PRIMARY KEY,
         question TEXT NOT NULL,
-        answerA TEXT NOT NULL,
-        answerB TEXT NOT NULL,
-        answerC TEXT NOT NULL,
-        answerD TEXT NOT NULL,
         correctanswer CHAR NOT NULL,
         avgtime FLOAT,
         numofanswers INT
     ) """
+
 sql_create_answers = """
     CREATE TABLE IF NOT EXISTS answers (
         id INT AUTO_INCREMENT PRIMARY KEY,
+        question_id INT NOT NULL,
+        shortanswer CHAR NOT NULL,
+        answer TEXT NOT NULL,
+        FOREIGN KEY (question_id) REFERENCES questions(id)
+    ) """
+
+sql_create_answers_given = """
+    CREATE TABLE IF NOT EXISTS answersgiven (
+        id INT AUTO_INCREMENT PRIMARY KEY,
         user_id INT NOT NULL,
         question_id INT NOT NULL,
-        answer CHAR NOT NULL,
+        answer_id INT NOT NULL,
         timeelapsed FLOAT NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (question_id) REFERENCES questions(id)
+        FOREIGN KEY (question_id) REFERENCES questions(id),
+        FOREIGN KEY (answer_id) REFERENCES answers(id)
     ) """
 
 cursor = database.cursor(buffered=True)
 cursor.execute(sql_create_users)
 cursor.execute(sql_create_questions)
 cursor.execute(sql_create_answers)
-
-totalaveragetime = 0
-oppositetime = 0
-suggestedquestionID = 0
-usertuple = 0
-questiontuple = 0
+cursor.execute(sql_create_answers_given)
 
 def getusertuple(name):
     sql = "SELECT * FROM users WHERE name = %s"
     cursor.execute(sql, (name,))
-    return cursor.fetchone()
-
-def getquestiontuple(id):
-    sql = "SELECT * FROM questions WHERE id = %s"
-    cursor.execute(sql, (id,))
     return cursor.fetchone()
 
 username = input("Type your user name: ")
@@ -70,36 +67,45 @@ if usertuple is None:
     database.commit()
     usertuple = getusertuple(username)
 
+totalaveragetime = 0
+oppositetime = 0
+suggestedquestionID = 0
+questiontuple = 0
+
+def getquestiontuple(id):
+    sql = "SELECT * FROM questions WHERE id = %s"
+    cursor.execute(sql, (id,))
+    return cursor.fetchone()
+
 cursor.execute("SELECT COUNT(numofanswers) FROM questions")
 numofanswers = cursor.fetchone()
 print("Numofanswers: ", numofanswers)
-if numofanswers[0] == 0: # Er zijn geen antwoorden
+if numofanswers[0] == 0: # Er zijn geen vragen gemaakt
     totalaveragetime = starttime
-    oppositetime = starttime
     suggestedquestionID = averagequestionID
     questiontuple = getquestiontuple(suggestedquestionID)
-else: # Er zijn antwoorden
+else: # Er zijn vragen gemaakt
     cursor.execute("SELECT SUM(avgtime*numofanswers) FROM questions")
     totaltime = cursor.fetchone()
+    print("Totaltime: ", totaltime)
     cursor.execute("SELECT SUM(numofanswers) FROM questions")
     totalnumofanswers = cursor.fetchone()
-    print("Totaltime: ", totaltime)
     print("Totalnumofanswers: ", totalnumofanswers)
     totalaveragetime = totaltime[0]/float(totalnumofanswers[0])
 
-    cursor.execute("SELECT id FROM questions WHERE avgtime IS NULL")
+    cursor.execute("SELECT id FROM questions WHERE numofanswers IS NULL")
     result = cursor.fetchone()
     if result is not None: # Er zijn niet gemaakte vragen
         suggestedquestionID = result[0]
         questiontuple = getquestiontuple(suggestedquestionID)
     else: # Alle vragen zijn gemaakt
-        if usertuple[2] is not None: # Gebruiker heeft vragen gemaakt
+        if usertuple[3] is not None: # Gebruiker heeft vragen gemaakt
             oppositetime = 2 * totalaveragetime - usertuple[2]
         else: # Gebruiker heeft geen vragen gemaakt
             oppositetime = totalaveragetime
 
         # Zoek de vraag het dichtst bij oppositetime (die niet al eerder gemaakt is door gebruiker)
-        sql = "SELECT * FROM questions WHERE id NOT IN (SELECT DISTINCT question_id FROM answers WHERE user_id = %s) ORDER BY ABS(%s - avgtime) ASC LIMIT 1"
+        sql = "SELECT * FROM questions WHERE id NOT IN (SELECT DISTINCT question_id FROM answersgiven WHERE user_id = %s) ORDER BY ABS(%s - avgtime) ASC LIMIT 1"
         cursor.execute(sql, (usertuple[0], oppositetime))
         questiontuple = cursor.fetchone()
         if questiontuple is None: print("You have answered all questions")
@@ -116,46 +122,59 @@ if questiontuple is None: exit()
 # Reken penalty uit
 penalty = 0
 avgnumofpenalties = 0
-if questiontuple[8] is None: # Vraag is niet eerder gemaakt
-    avgnumofpenalties = 1.5
-    penalty = totalaveragetime/avgnumofpenalties
-else: # Vraag is eerder gemaakt
-    sql = "SELECT COUNT(answer) FROM answers WHERE question_id = %s"
-    cursor.execute(sql, (questiontuple[0],))
-    answers = cursor.fetchone()
-    correctanswers = questiontuple[8]
-    errors = answers[0] - correctanswers
-    if errors == 0: # Nog geen fouten gemaakt bij vraag
-        avgnumofpenalties = 1.5
-    else:   # Wel eerder fouten gemaakt
-        avgnumofpenalties = errors / correctanswers
-    penalty = questiontuple[7] / avgnumofpenalties
+sql = "SELECT COUNT(answer) FROM answers WHERE question_id = %s"
+cursor.execute(sql, (questiontuple[0],))
+answers = cursor.fetchone()
+avgnumofpenalties = (answers[0] - 1) / 2
+if questiontuple[3] is None: penalty = totalaveragetime/avgnumofpenalties
+else: penalty = questiontuple[3]/avgnumofpenalties
 print("Penalty: ", penalty)
 print("Avgnumofpenalties: ", avgnumofpenalties)
 
 # Vraag maken
-answer = 0
-answers = []
-totaltime = 0
-while answer != questiontuple[6]:
-    answer = input("Submit your answer: ")
-    timeelapsed = round(float(input("Submit time elapsed before answering: ")), 1)
-    answers.append((usertuple[0], questiontuple[0], answer, timeelapsed))
-    print("Answers: ", answers)
-    totaltime += timeelapsed
-    if answer != questiontuple[6]: totaltime += penalty
-sql = "INSERT INTO answers (user_id, question_id, answer, timeelapsed) VALUES (%s, %s, %s, %s)"
-cursor.executemany(sql, answers)
-database.commit()
+sql = "SELECT * FROM answers WHERE question_id = %s"
+cursor.execute(sql, (questiontuple[0],))
+answers = cursor.fetchall()
+print("Answers: ", answers)
 
+shortanswer = 0
+shortanswersgiven = []
+answersgiven = []
+totaltime = 0
+timeelapsed = 0
+while shortanswer != questiontuple[2]:
+    if len(answers) - 1 == len(answersgiven):
+        shortanswer = questiontuple[2]
+        timeelapsed = 0
+    else:
+        shortanswer = input("Submit your answer: ")
+        timeelapsed = round(float(input("Submit time elapsed before answering: ")), 1)    
+    if shortanswersgiven.count(shortanswer) == 1:
+        print("Earlier answer submitted")
+        continue
+    else:
+        shortanswersgiven.append(shortanswer)
+        totaltime += timeelapsed
+        if shortanswer != questiontuple[2]: totaltime += penalty
+
+    sql = "SELECT id FROM answers WHERE question_id = %s AND shortanswer = %s"
+    cursor.execute(sql, (questiontuple[0], shortanswer))    
+    answer_id = cursor.fetchone()
+    answersgiven.append((usertuple[0], questiontuple[0], answer_id[0], timeelapsed))
+    print("Answers given: ", answersgiven)
+
+sql = "INSERT INTO answersgiven (user_id, question_id, answer_id, timeelapsed) VALUES (%s, %s, %s, %s)"
+cursor.executemany(sql, answersgiven)
+database.commit()
+    
 # Database updaten
 sql = "UPDATE questions SET avgtime = %s, numofanswers = %s WHERE id = %s"
-if questiontuple[7] is None:
+if questiontuple[3] is None:
     cursor.execute(sql, (totaltime, 1, questiontuple[0]))
     database.commit()
 else:
-    numofanswers = questiontuple[8]
-    avgtime = questiontuple[7]
+    numofanswers = questiontuple[4]
+    avgtime = questiontuple[3]
     totalanswertime = (numofanswers * avgtime + totaltime) / (numofanswers + 1)
     numofanswers += 1
     totalanswertime = round(totalanswertime, 1)
--- sample1.sql
+++ sample1.sql
@@ -24,28 +24,55 @@ USE `sample1`;
 DROP TABLE IF EXISTS `answers`;
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int NOT NULL,
+  `shortanswer` char(1) NOT NULL,
+  `answer` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table sample1.answers: ~14 rows (approximately)
+REPLACE INTO `answers` (`id`, `question_id`, `shortanswer`, `answer`) VALUES
+	(1, 1, 'A', 'Alfabet'),
+	(2, 1, 'B', 'Bal'),
+	(3, 2, 'A', 'Alfabet'),
+	(4, 2, 'B', 'Bal'),
+	(5, 2, 'C', 'Cool'),
+	(6, 3, 'A', 'Alfabet'),
+	(7, 3, 'B', 'Bal'),
+	(8, 3, 'C', 'Cool'),
+	(9, 3, 'D', 'Dweil'),
+	(10, 4, 'A', 'Alfabet'),
+	(11, 4, 'B', 'Bal'),
+	(12, 4, 'C', 'Cool'),
+	(13, 4, 'D', 'Dweil'),
+	(14, 4, 'E', 'Elfje');
+
+-- Dumping structure for table sample1.answersgiven
+DROP TABLE IF EXISTS `answersgiven`;
+CREATE TABLE IF NOT EXISTS `answersgiven` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `question_id` int NOT NULL,
-  `answer` char(1) NOT NULL,
+  `answer_id` int NOT NULL,
   `timeelapsed` float NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `question_id` (`question_id`),
-  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
+  KEY `answer_id` (`answer_id`),
+  CONSTRAINT `answersgiven_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `answersgiven_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `answersgiven_ibfk_3` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table sample1.answers: ~0 rows (approximately)
+-- Dumping data for table sample1.answersgiven: ~0 rows (approximately)
 
 -- Dumping structure for table sample1.questions
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE IF NOT EXISTS `questions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `question` text NOT NULL,
-  `answerA` text NOT NULL,
-  `answerB` text NOT NULL,
-  `answerC` text NOT NULL,
-  `answerD` text NOT NULL,
   `correctanswer` char(1) NOT NULL,
   `avgtime` float DEFAULT NULL,
   `numofanswers` int DEFAULT NULL,
@@ -53,11 +80,11 @@ CREATE TABLE IF NOT EXISTS `questions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table sample1.questions: ~4 rows (approximately)
-REPLACE INTO `questions` (`id`, `question`, `answerA`, `answerB`, `answerC`, `answerD`, `correctanswer`, `avgtime`, `numofanswers`) VALUES
-	(1, 'A', 'A', 'B', 'C', 'D', 'A', NULL, NULL),
-	(2, 'B', 'A', 'B', 'C', 'D', 'B', NULL, NULL),
-	(3, 'C', 'A', 'B', 'C', 'D', 'C', NULL, NULL),
-	(4, 'D', 'A', 'B', 'C', 'D', 'D', NULL, NULL);
+REPLACE INTO `questions` (`id`, `question`, `correctanswer`, `avgtime`, `numofanswers`) VALUES
+	(1, 'A?', 'A', NULL, NULL),
+	(2, 'B?', 'B', NULL, NULL),
+	(3, 'C?', 'C', NULL, NULL),
+	(4, 'D?', 'D', NULL, NULL);
 
 -- Dumping structure for table sample1.users
 DROP TABLE IF EXISTS `users`;
@@ -68,9 +95,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `numofquestions` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table sample1.users: ~0 rows (approximately)
+-- Dumping data for table sample1.users: ~4 rows (approximately)
+REPLACE INTO `users` (`id`, `name`, `avgtime`, `numofquestions`) VALUES
+	(1, 'Jos', NULL, NULL),
+	(2, 'Jan', NULL, NULL),
+	(3, 'Stefan', NULL, NULL),
+	(4, 'Korian', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
